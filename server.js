27945de0b35e428eda1e1db8a5e72c466ed80a21
Trevor McLaurine <trevor.mclaurine@gmail.com>
2022-11-23 00:57:54 -0600

    env....

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config({ path: './.env' });
+require('dotenv').config();
 const cors = require('cors')
 
 const quizRoutes = require('./router/quiz');
